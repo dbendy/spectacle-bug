75d70d268bb564f99e7646cb1d83b30427a985a9
Daniel Bendavid <dbendy@gmail.com>
2019-05-13 09:06:18 -0400

    Add code to demonstrate bug.

--- src/presentation.js
+++ src/presentation.js
@@ -3,15 +3,11 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
-  ListItem,
-  List,
-  Quote,
   Slide,
   Text,
+  GoToAction,
 } from 'spectacle';
 
 // Import theme
@@ -37,58 +33,34 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
+        transition={[]}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
+        <Slide>
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Spectacle Bug
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide id='toc'>
           <Heading size={6} textColor="primary" caps>
-            Typography
+            Table of Contents
           </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+          <div><GoToAction slide={"yellow"} bgColor="yellow">Yellow Slides</GoToAction></div>
+          <div><GoToAction slide={"green"} bgColor="green">Green Slides</GoToAction></div>
+          <div><GoToAction slide={"blue"} bgColor="blue">Blue Slides</GoToAction></div>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
+        <Slide id="yellow" bgColor="yellow"><Text>Yellow Slides</Text></Slide>
+        <Slide bgColor="yellow"><Text>Yellow 1</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="yellow"><Text>Yellow 2</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="yellow"><Text>Yellow 3</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide id="green" bgColor="green"><Text>Green Slides</Text></Slide>
+        <Slide bgColor="green"><Text>Green 1</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="green"><Text>Green 2</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="green"><Text>Green 3</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide id="blue" bgColor="blue"><Text>Blue Slides</Text></Slide>
+        <Slide bgColor="blue"><Text>Blue 1</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="blue"><Text>Blue 2</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
+        <Slide bgColor="blue"><Text>Blue 3</Text><GoToAction slide="toc">Table of Contents</GoToAction></Slide>
       </Deck>
     );
   }
